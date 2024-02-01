7f10311ff5f2982fb8a9e4d9c245369188edeb5e
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2024-02-01 08:46:26 -0500

    Added PLD run files.

---
+++ runBatchZonalHist_GLAKES.py
@@ -0,0 +1,82 @@
+import matplotlib.patches as mpatches
+from seaborn import objects as so
+import os
+from pathlib import Path
+# import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import numpy as np
+from scipy.stats import binned_statistic
+
+from retry import retry
+# import geopandas as gpd
+# import pandas as pd
+# import dask.dataframe as dd
+# import ee
+# import geemap
+# from matplotlib import pyplot as plt
+# import seaborn as sns
+# import pyogrio
+# from tqdm import tqdm
+from util import *
+
+## I/O
+# modN = 300000
+# analysis_dir = '/Volumes/thebe/Ch4/GSW_zonal_stats/HL/vtest/'
+analysis_dir = '/Volumes/metis/Datasets/Liu_aq_veg/Zonal-hist'
+# index_file = '/Volumes/thebe/Other/Kuhn-olefeldt-BAWLD/BAWLD/BAWLD_V1___Shapefile.zip'
+# ee_zones_pth = "projects/sat-io/open-datasets/HydroLakes/lake_poly_v10"
+ee_zones_pths = ['projects/ee-ekyzivat/assets/Shapes/GLAKES/GLAKES_na2',
+                 'projects/ee-ekyzivat/assets/Shapes/GLAKES/GLAKES_as',
+                 'projects/ee-ekyzivat/assets/Shapes/GLAKES/GLAKES_eu',
+                 'projects/ee-ekyzivat/assets/Shapes/GLAKES/GLAKES_na1']  # 'projects/ee-ekyzivat/assets/Shapes/GLAKES/GLAKES_na1'
+ee_value_raster_pth = "JRC/GSW1_4/GlobalSurfaceWater"
+nWorkers = 30
+# crs_str = 'PROJCS["Lambert_Azimuthal_Equal_Area",GEOGCS["Unknown",DATUM["D_unknown",SPHEROID["Unknown",6371007.181,0]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Lambert_Azimuthal_Equal_Area"],PARAMETER["latitude_of_origin",45.5],PARAMETER["central_meridian",-114.125],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1]]'
+crs_wkt = 'PROJCS["ProjWiz_Custom_Lambert_Azimuthal", GEOGCS["GCS_WGS_1984", DATUM["D_WGS_1984", SPHEROID["WGS_1984",6378137.0,298.257223563]], PRIMEM["Greenwich",0.0], UNIT["Degree",0.0174532925199433]], PROJECTION["Lambert_Azimuthal_Equal_Area"], PARAMETER["False_Easting",0.0], PARAMETER["False_Northing",0.0], PARAMETER["Central_Meridian",0], PARAMETER["Latitude_Of_Origin",65], UNIT["Meter",1.0]]'
+
+# name_lat = 'Pour_lat'
+# name_lon = 'Pour_long'
+name_lat = 'Lat'
+name_lon = 'Lon'
+lat_ranges = [[40, 78.0], [40, 78.0], [40.0, 77.0], [40, 78.0]]
+lon_ranges = [[-180, 180], [-180, 180], [-24.5, 69.0], [-180, 180]]
+# lat_range = [62, 64.5]  # for testing
+# lon_range = [-105, -103]
+step = 0.5
+offset_lower = 0  # 0.25
+
+## Geemap zonal histogram parameters (note: start small and only increase them if API is hitting memory limits and not returning a CSV file)
+scale = None  # 30
+tile_scale = 12  # 2
+
+## I/O for reading csvs
+id_var = 'Lake_id'  # Hylak_id
+area_var = 'Area_PW'  # Lake_area # km2
+# '/Volumes/thebe/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10.shp'
+lake_inventory_pth = '/Volumes/metis/Datasets/GLAKES/GLAKES.gdb'
+loadJoined = False
+
+# Auto I/O
+offset_upper = step
+
+## Testing
+# vect = ee.FeatureCollection("projects/sat-io/open-datasets/HydroLakes/lake_poly_v10").map(addMod)
+# print(vect.filter("Hylak_id < 500").filter("Lake_area < 1000").size().getInfo())
+# print('Number of features in chunk: ', vect.filter("Hylak_id < 1000").size())
+# vect.first().get('mod50')
+# vect.propertyNames()
+# vect.first().propertyNames() # to actually print the result!
+# vect.get('mod50')
+
+## Test on single (Error: property 'element' is required means some filter returned zero. )
+# getResult(3, 1)
+# getResult(0, np.array([-104.25, 51.25]))
+
+######################
+#### Operations
+######################
+# runGlakesByRegion(ee_zones_pths, lat_ranges, lon_ranges, step, analysis_dir, name_lat, name_lon,
+#                   offset_upper, offset_lower, crs_wkt, scale, tile_scale, ee_value_raster_pth, nWorkers)
+
+loadJoined = True
+CombineProcessGlakes(analysis_dir, ee_zones_pths, loadJoined, id_var)
